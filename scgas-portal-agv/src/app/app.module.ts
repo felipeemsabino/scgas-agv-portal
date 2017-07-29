6c64329aad0b53880b9f38b5716d45b884d81e05
Felipe Sabino <felipeems87@gmail.com>
2017-07-29 15:25:27 +0100

    Commitando tela inicial de login. Commitando jquery e bootstrap.

--- scgas-portal-agv/src/app/app.module.ts
+++ scgas-portal-agv/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+
+
+const appRoutes: Routes = [
+  {
+    path:'', component: LoginComponent
+  }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
