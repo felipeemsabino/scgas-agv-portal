4a2f9451fe929a294d40cdede3d7fbe8294c1a6b
Felipe Sabino <felipeems87@gmail.com>
2017-07-29 16:10:12 +0100

    Commitando tela de login. Commitando tela home. Commitando componente header.

--- scgas-portal-agv/src/app/app.module.ts
+++ scgas-portal-agv/src/app/app.module.ts
@@ -4,9 +4,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 
 
-const appRoutes: Routes = [
+const appRoutes: Routes = [{
+      path: 'home',
+      children: [
+        /*{ path: 'postos', component: PostosComponent },
+        { path: 'postos', component: PostosComponent },
+        { path: 'noticias', component: NoticiasComponent },
+        { path: 'usuarios', component: UsuariosComponent },
+        { path: 'parametros-gerais', component: ParametrosGeraisComponent },
+        { path: 'temp2', component: TempComponent2 },*/
+      ],
+      component: HomeComponent
+    },
   {
     path:'', component: LoginComponent
   }
@@ -16,7 +29,9 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
